067af6d9208826ba0f2755c2a07563f9e3a5cab4
ZIEGLER Aurelie (SNCF VOYAGEURS / DIRECTION DE LIGNE ALSACE / EQ ESC-VEN MUL) <8301681s@commun.ad.sncf.fr>
2023-04-17 14:20:26 +0200

    nouvel index

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ console.log(userInfo);
 var cowsay = require("cowsay");
 
 console.log(cowsay.say({
-    text : "I'm a moooodule",
+    text : `Je m'appelle ${userInfo.nom} et j'étudie à ${userInfo.campus}`,
     e : "oO",
     T : "U "
 }));
